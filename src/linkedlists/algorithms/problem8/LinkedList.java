5166cc8433198df44da8beb119c9db258c285664
ketandikshit <kdcool6932@yahoo.com>
2015-04-20 13:28:39 +0200

    algos updated

--- src/linkedlists/algorithms/problem8/LinkedList.java
+++ src/linkedlists/algorithms/problem8/LinkedList.java
@@ -130,6 +130,141 @@ public class LinkedList<Item> {
 		System.out.println("************ IS PALLINDROME ***********");
 		return true;
 	}
+
+	/**
+	 * problem8
+	 * Function to check if a singly linked list is palindrome
+	 * 1) Get the middle of the linked list.
+	 * 2) Reverse the second half of the linked list.
+	 * 3) Check if the first half and second half are identical.
+	 * 4) Construct the original linked list by reversing the second half again
+	 * and attaching it back to the first half
+	 * 
+	 * To divide the list in two halves.
+	 * When number of nodes are even, the first and second half contain exactly
+	 * half nodes.
+	 * The challenging thing in this method is to handle the case when number of
+	 * nodes are odd.
+	 * We don’t want the middle node as part of any of the lists as we are going
+	 * to compare them for equality.
+	 * For odd case, we use a separate variable ‘midnode’.
+	 * 
+	 * This method takes O(n) time and O(1) extra space.
+	 * 
+	 * @createdOn 18-Apr-2015 3:45:42 pm
+	 * @author ketandikshit
+	 * @param linkedList
+	 * @return
+	 */
+	public boolean checkPalindromeByReversingSecondHalf(
+			LinkedList<Item> linkedList) {
+
+		Node slowCntr = linkedList.first;
+		Node fastCntr = linkedList.first;
+
+		Node prevOfSlowPtr = null;
+		boolean isPallindrome = false;
+		if ((linkedList.first != null) && (linkedList.first.next != null)) {
+			/*
+			 * Get the middle of the list. Move slow_ptr by 1
+			 * and fast_ptrr by 2, slow_ptr will have the middle
+			 * node
+			 */
+			while ((fastCntr != null) && (fastCntr.next != null)) {
+				/*
+				 * We need previous of the slow_ptr for
+				 * linked lists with odd elements
+				 */
+				prevOfSlowPtr = slowCntr;
+				slowCntr = slowCntr.next;
+				fastCntr = fastCntr.next.next;
+			}
+
+			Node midNode = null;
+
+			/*
+			 * fast_ptr would become NULL when there are even elements in list.
+			 * And not NULL for odd elements. We need to skip the middle node
+			 * for odd case and store it somewhere so that we can restore the
+			 * original list
+			 */
+			if (fastCntr != null) {
+				midNode = slowCntr;
+				slowCntr = slowCntr.next;
+			}
+
+			Node second_half = null;
+			// Now reverse the second half and compare it with first half
+			second_half = slowCntr;
+			prevOfSlowPtr.next = null; // NULL terminate first half
+			second_half = reverse(second_half);
+			isPallindrome = compareLists(first, second_half);
+
+			/* Construct the original list back */
+			reverse(second_half); // Reverse the second half again
+			/*
+			 * If there was a mid node (odd size case) which
+			 * was not part of either first half or second
+			 * half.
+			 */
+			if (midNode != null) {
+				prevOfSlowPtr.next = midNode;
+				midNode.next = second_half;
+			} else {
+				prevOfSlowPtr.next = second_half;
+			}
+		}
+
+		return isPallindrome;
+
+	}
+	/**
+	 * Reverses the linkedlist
+	 * 
+	 * @createdOn 18-Apr-2015 5:34:18 pm
+	 * @author ketandikshit
+	 * @param head
+	 *            the head of linkedlist
+	 * @return the head of the reversed linkedlist
+	 */
+	private Node reverse(Node head) {
+
+		Node prev = null;
+		Node current = head;
+		Node next = null;
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		head = prev;
+		return head;
+	}
+
+	private boolean compareLists(Node head1, Node head2) {
+		Node temp1 = head1;
+		Node temp2 = head2;
+
+		while ((temp1 != null) && (temp2 != null)) {
+			if (temp1.item == temp2.item) {
+				temp1 = temp1.next;
+				temp2 = temp2.next;
+			} else
+				return false;
+		}
+
+		/* Both are empty reurn 1 */
+		if (temp1 == null && temp2 == null)
+			return true;
+
+		/*
+		 * Will reach here when one is NULL
+		 * and other is not
+		 */
+		return false;
+	}
+
 	/**
 	 * @createdOn 17-Apr-2015 11:52:58 pm
 	 * @author ketandikshit
@@ -147,7 +282,11 @@ public class LinkedList<Item> {
 
 			count++;
 		}
+		System.out.println(linkedList);
 		System.out.println(linkedList.checkPalindromeUsingStack(linkedList));
+		System.out.println("\n");
+		System.out.println(linkedList
+				.checkPalindromeByReversingSecondHalf(linkedList));
 
 	}
 }
