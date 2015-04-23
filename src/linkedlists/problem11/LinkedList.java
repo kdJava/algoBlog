36fb7ab1afa226c85cad5621656b8d14aacb937a
ketandikshit <kdcool6932@yahoo.com>
2015-04-24 00:02:35 +0200

    problem 10 and problem 11 commit

--- src/linkedlists/problem11/LinkedList.java
+++ src/linkedlists/problem11/LinkedList.java
@@ -1,16 +1,16 @@
 /**
- * @createdOn 22-Apr-2015 11:29:52 pm
- * @qualifiedName GeekyAlgoJava/linkedlists.algorithms.problem10/LinkedList.java
+ * @createdOn 23-Apr-2015 10:31:25 pm
+ * @qualifiedName GeekyAlgoJava/linkedlists.problem11/LinkedList.java
  * @author ketandikshit
  * @typeName LinkedList
  * @year 2015
  */
-package linkedlists.algorithms.problem10;
+package linkedlists.problem11;
 
 /**
  * @author ketandikshit
- * @createdOn 22-Apr-2015 11:29:52 pm
- * @qualifiedName GeekyAlgoJava/linkedlists.algorithms.problem10/LinkedList.java
+ * @createdOn 23-Apr-2015 10:31:25 pm
+ * @qualifiedName GeekyAlgoJava/linkedlists.problem11/LinkedList.java
  * @year 2015
  */
 public class LinkedList<Item extends Comparable<Item>> {
@@ -117,77 +117,118 @@ public class LinkedList<Item extends Comparable<Item>> {
 	}
 
 	/**
-	 * problem10
-	 * Remove duplicates from a sorted linked list
-	 * Algorithm:
-	 * 1. Traverse the list from the head (or start) node. While traversing.
-	 * 2. Compare each node with its next node.
-	 * 3. If data of next node is same as current node then delete the next
-	 * node.
-	 * 4. Before we delete a node, we need to store next pointer of the node
+	 * problem11
+	 * Remove duplicates from an unsorted linked list
+	 * Using two loops; (trivial)
+	 * This is the simple way where two loops are used.
+	 * Outer loop is used to pick the elements one by one and inner loop
+	 * compares the picked element with rest of the elements.
 	 * 
-	 * @createdOn 23-Apr-2015 12:07:47 am
+	 * @createdOn 23-Apr-2015 10:44:39 pm
 	 * @author ketandikshit
-	 * @param linkedList
-	 *            sorted linked-list from which the duplicates need to be
-	 *            removed
+	 * @param linkedlist
+	 *            the unsorted linked list to remove duplicates from
 	 */
-	public void removeDuplicatesFromSortedLinkedList(LinkedList<Item> linkedList) {
-		/* Pointer to traverse the linked list */
-		Node currentNode = linkedList.first;
-
-		/* Pointer to store the next pointer of a node to be deleted */
-		Node nextOfNextNode;
-
-		/* do nothing if the list is empty */
-		if (currentNode == null)
-			return;
-
-		/* Traverse the list till last node */
-		while (currentNode.next != null) {
-			/* Compare current node with next node */
-			if (currentNode.item.compareTo(currentNode.next.item) == 0) {
-				/* The sequence of steps is important */
-				nextOfNextNode = currentNode.next.next;
-				currentNode.next = nextOfNextNode;
-				size--;
-			} else /* This is tricky: only advance if no deletion */
-			{
-				currentNode = currentNode.next;
+	public void removeDuplicatesUsingDoubleLoops(LinkedList<Item> linkedlist) {
+		Node currentNode1 = linkedlist.first;
+		while (currentNode1 != null) {
+			Node curreNode2 = currentNode1;
+			while (curreNode2.next != null) {
+				if (currentNode1.item.equals(curreNode2.next.item)) {
+					Node duplicate = curreNode2.next;
+					curreNode2.next = curreNode2.next.next;
+					duplicate = null;
+					size--;
+				} else {
+					curreNode2 = curreNode2.next;
+				}
 			}
+			currentNode1 = currentNode1.next;
+		}
+	}
+
+	private Node mergeSortList(Node head) {
+
+		if (head == null || head.next == null)
+			return head;
+		Node left = head;
+		Node right = head.next;
+		while ((right != null) && (right.next != null)) {
+			head = head.next;
+			right = (right.next).next;
+		}
+		right = head.next;
+		head.next = null;
+		return merge(mergeSortList(left), mergeSortList(right));
+	}
+
+	private Node merge(Node leftList, Node rightList) {
+		Node temp = new Node();
+		Node head = temp;
+		Node currentNode = head;
+		while ((leftList != null) && (rightList != null)) {
+			if (leftList.item.compareTo(rightList.item) <= 0) {
+				currentNode.next = leftList;
+				currentNode = leftList;
+				leftList = leftList.next;
+			} else {
+				currentNode.next = rightList;
+				currentNode = rightList;
+				rightList = rightList.next;
+			}
+		}
+		currentNode.next = (leftList == null) ? rightList : leftList;
+		return head.next;
+	}
+
+	private void mergeSortTheList(LinkedList<Item> linkedList) {
+		Node head = mergeSortList(linkedList.first);
+
+		Node currNode = head;
+		while (currNode != null) {
+			System.out.println(currNode.item);
+			currNode = currNode.next;
 		}
 	}
 
 	/**
-	 * @createdOn 22-Apr-2015 11:29:52 pm
+	 * @createdOn 23-Apr-2015 10:31:25 pm
 	 * @author ketandikshit
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList = new LinkedList<Integer>();
-
 		linkedList.addAtHead(10);
 		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(10);
 		linkedList.addAtEnd(20);
-		linkedList.addAtEnd(20);
+		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(30);
 		linkedList.addAtEnd(30);
 		linkedList.addAtEnd(40);
+		linkedList.addAtEnd(10);
 		linkedList.addAtEnd(50);
+		linkedList.addAtEnd(10);
 		linkedList.addAtEnd(50);
+		linkedList.addAtEnd(10);
 		linkedList.addAtEnd(50);
-		linkedList.addAtEnd(60);
-		linkedList.addAtEnd(70);
-		linkedList.addAtEnd(70);
-		linkedList.addAtEnd(80);
-		linkedList.addAtEnd(90);
-		linkedList.addAtEnd(100);
-		linkedList.addAtEnd(100);
-		linkedList.addAtEnd(100);
+		linkedList.addAtEnd(10);
+		linkedList.addAtEnd(50);
 
 		System.out.println(linkedList);
-
-		linkedList.removeDuplicatesFromSortedLinkedList(linkedList);
+		linkedList.removeDuplicatesUsingDoubleLoops(linkedList);
 		System.out.println(linkedList);
+
+		LinkedList<Integer> linkedList2 = new LinkedList<Integer>();
+		linkedList2.addAtHead(100);
+		linkedList2.addAtEnd(80);
+		linkedList2.addAtEnd(60);
+		linkedList2.addAtEnd(50);
+
+		linkedList2.mergeSortTheList(linkedList2);
 
 	}
 
